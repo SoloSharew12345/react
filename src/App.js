ca4684f5c10473e7637b59469fb21fd83301bf17
SoloSharew12345 <153470347+SoloSharew12345@users.noreply.github.com>
2024-01-01 09:50:31 -0500

    Install tailwind css and integrating to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
 
@@ -7,7 +7,7 @@ function App() {
   const [role,setRole] = useState('dev');
   const showEmployees = true;
   return (
-    <div className="App">
+    <div className="App bg-red-300">
       {showEmployees ? ( 
       <>
       <input 
